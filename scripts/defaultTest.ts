14aaad338834fb48f3657b2d3d41ad517fc66297
fanyushun <fanyushun@yunwutech.com>
2024-07-17 11:18:07 +0800

    feat: 测试文件增加 js 测试

--- scripts/defaultTest.ts
+++ scripts/defaultTest.ts
@@ -1,9 +1,14 @@
 function getDefaultTest (name: string, type: string): string {
   return `import { expect, test } from 'vitest'
 import { ${name} } from '../../src/${type}/${name}/typescript.ts'
+import { ${name} as ${name}Js } from '../../src/${type}/${name}/javascript.js'
 
-test('', () => {
+test(\`${name}\`, () => {
   expect(${name}()).toBe('')
+})
+
+test(\`${name}Js\`, () => {
+  expect(${name}Js()).toBe('')
 })
 `
 }
